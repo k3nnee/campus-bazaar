9912d4bbd309c9ad105edc10d6fb8b2d27ddfb85
Kenny <ktang3958@gmail.com>
2024-12-09 02:05:50 -0500

    Removed URL

--- backend/utils/mongoclient.js
+++ backend/utils/mongoclient.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient("mongodb+srv://admin:ihavealongpassword@campus-bazaar.mv6nz.mongodb.net/");
+const client = new MongoClient(uri);
 
 client.connect().then(async () => {
     console.log("Connected to the database");
